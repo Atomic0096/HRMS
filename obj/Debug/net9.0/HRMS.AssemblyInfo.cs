74e53e67a5b0510c8ee041ec57803a2d5337eabd
Suica <atomic2974@gmail.com>
2025-02-27 19:19:06 +0800

    添加了必要的NuGet包

--- obj/Debug/net9.0/HRMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/HRMS.AssemblyInfo.cs
@@ -10,11 +10,12 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-HRMS-81c028de-b81d-452d-bbee-519736045329")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79a74e660c22a2e9dc6e48d56b3216c20c2a248")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
