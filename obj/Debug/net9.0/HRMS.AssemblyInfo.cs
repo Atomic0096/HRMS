2fe01430d6d37f3a60c42791f61e2c5ba70e562d
Suica <atomic2974@gmail.com>
2025-04-26 12:58:32 +0800

    完成了财务端的报销管理

--- obj/Debug/net9.0/HRMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/HRMS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7f342c45ce2c5f470fb027295a6f43489cc1e83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42dce35941bbc44b17166c306031fb208b7d865c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
